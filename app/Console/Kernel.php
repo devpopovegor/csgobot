788a872add0ee2e15cc4973f1d0982a9eeb42eba
devpopovegor <popovegordev@mail.ru>
2017-10-29 15:29:51 +0300

    add test command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\FillItemsCommand;
+use App\Console\Commands\TestCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        FillItemsCommand::class
+        FillItemsCommand::class,
+        TestCommand::class
     ];
 
     /**
