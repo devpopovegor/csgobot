1c5104c1180b894dd36155a29a7f5d25c1b16647
devpopovegor <popovegordev@mail.ru>
2017-11-11 17:51:49 +0300

    123

--- app/Console/Commands/CsMoney.php
+++ app/Console/Commands/CsMoney.php
@@ -120,7 +120,7 @@ class CsMoney extends Command
                                 ->where('value', '=', $pattern)->first()) {
                                 Telegram::sendMessage([
                                     'chat_id' => $task->chat_id,
-                                    'text' => "{$task->item->name}\r\n{$csmoney->url}\r\n{$task->item->phase}\r\n{$obj->f[0]}\r\n<a href='$url_metjm'>metjm</a>",
+                                    'text' => "{$task->item->name}\r\n{$csmoney->url}\r\n{$task->item->phase}\r\n{$obj->f[0]}\r\n{$pattern}\r\n<a href='$url_metjm'>metjm</a>",
                                     'parse_mode' => 'HTML'
                                 ]);
                                 $task->delete();
