37a2a6405b991ea2052b29c3571e921031621f29
devpopovegor <popovegordev@mail.ru>
2017-11-19 13:08:32 +0300

    123

--- app/Console/Commands/Check.php
+++ app/Console/Commands/Check.php
@@ -40,7 +40,7 @@ class Check extends Command
     {
 //        $this->call( 'raffle:check', [ 'site_id' => 1 ] );
 //        $this->call( 'raffle:check', [ 'site_id' => 2 ] );
-//        $this->call( 'raffle:check', [ 'site_id' => 3 ] );
+        $this->call( 'raffle:check', [ 'site_id' => 3 ] );
         $this->call( 'cstrade:check');
 //        $this->call( 'skintrade:check');
 //        $this->call( 'csgosum:check');
