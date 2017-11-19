f1f38370ba4d2bc1a24dc551bff4de6b5d42dc37
devpopovegor <popovegordev@mail.ru>
2017-11-19 17:57:25 +0300

    123

--- app/Commands/SearchCommand.php
+++ app/Commands/SearchCommand.php
@@ -185,6 +185,26 @@ class SearchCommand extends Command
                                         );
                                         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
                                         $curl_response = json_decode(curl_exec($curl));
+                                    } elseif ($mSite->id == 14){
+                                        curl_setopt($curl, CURLOPT_POST, true);
+                                        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                                            "Host: www.thecsgobot.com",
+                                            "Connection: keep-alive",
+                                            "Content-Length: 7",
+                                            "Accept: */*",
+                                            "Origin: https://www.thecsgobot.com",
+                                            "X-Requested-With: XMLHttpRequest",
+                                            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+                                            "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+                                            "Referer: https://www.thecsgobot.com/trading/",
+                                            "Accept-Language: en-US,en;q=0.9",
+                                            "Cookie: __cfduid=d55eded162019ba018906e0d894d5ea701511096482;
+            JSESSIONID=6C5A1642AAF4F09151B09838EDBBF944;
+            _ga=GA1.2.1308276030.1511096483;
+            _gid=GA1.2.437421720.1511096483"
+                                        ));
+                                        curl_setopt($curl, CURLOPT_POSTFIELDS, "who=bot");
+                                        $curl_response = json_decode(curl_exec($curl))->items;
                                     }
                                     else {
                                         $curl_response = json_decode(curl_exec($curl));
@@ -226,6 +246,9 @@ class SearchCommand extends Command
                                             break;
                                         case 13:
                                             $response = $this->check_csgosell($obj, $curl_response);
+                                            break;
+                                        case 14:
+                                            $response = $this->check_csgobot($obj, $curl_response);
                                             break;
                                     }
                                     curl_close($curl);
@@ -939,6 +962,60 @@ class SearchCommand extends Command
         return false;
     }
 
+    private function check_csgobot($obj, $curl_response){
+        $name = $obj->name;
+        if ($obj->phase){
+            if (strpos($name, '(') !== false){
+                $status = substr($name, strpos($name, '('));
+                $new_part = "({$obj->phase}) {$status}";
+                $name = substr_replace($name, $new_part, strpos($name, '('));
+            } else {
+                $name .= " ({$obj->phase})";
+            }
+        }
+
+        $items = collect($curl_response);
+        $items = $items->where('market_hash_name', '=', $name);
+        if ($obj->float) $items->where('descriptions.0.aFloat', '<=', $obj->float);
+        if (count($items)){
+            if ($obj->pattern){
+                foreach ($items as $item){
+                    $url_metj = "https://metjm.net/shared/screenshots-v5.php?cmd=request_new_link&inspect_link={$item->descriptions[0]->inspectURL}";
+                    $curl = curl_init();
+                    curl_setopt($curl, CURLOPT_URL, $url_metj);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    $response = json_decode($response);
+                    $pattern = null;
+                    $link_metjm = '';
+                    try {
+                        if ($response->success) {
+                            $pattern = $response->result->item_paintseed;
+                            $link_metjm = "https://metjm.net/csgo/#" . explode('%20', $item->descriptions[0]->inspectURL)[1];
+                        }
+                    } catch (\Exception $exception) {
+                        continue;
+                    }
+                    $need_item = Item::find($obj->id);
+                    $patterns = $need_item->patterns->where('name', '=', $obj->pattern)->where('value', '=', $pattern)->first();
+                    if ($patterns) {
+                        $this->replyWithChatAction(['action' => Actions::TYPING]);
+                        $this->replyWithMessage(['text' => "{$obj->name}\r\n{$obj->url}\r\n{$obj->phase}\r\n{$item->descriptions[0]->aFloat}\r\n{$obj->pattern}\r\n<a href='{$link_metjm}'>metjm</a>",
+                            'parse_mode' => 'HTML']);
+                        return true;
+                    }
+                }
+            } else {
+                $item = $items->first();
+                $metj = "https://metjm.net/csgo/#" . explode('%20', $item->descriptions[0]->inspectURL)[1];
+                $this->replyWithChatAction(['action' => Actions::TYPING]);
+                $this->replyWithMessage(['text' => "{$obj->name}\r\n{$obj->url}\r\n{$obj->phase}\r\n{$item->descriptions[0]->aFloat}\r\n<a href='{$metj}'>metjm</a>",
+                    'parse_mode' => 'HTML']);
+                return true;
+            }
+        }
+    }
 
     private function getDataMetjm($item)
     {
--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -22,35 +22,36 @@ class TelegramController extends Controller
     }
 
     public function test(){
-//        dd(strpos('★ Sport Gloves | Pandora\'s Box (Minimal Wear)', '\''));
-//        dd(urldecode("★ Sport Gloves | Pandora%27s Box (Minimal Wear)"));
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,"https://csgosell.com/phpLoaders/forceBotUpdate/all.txt");
+        curl_setopt($curl, CURLOPT_URL,"https://www.thecsgobot.com/api/service.inventory.json");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "Accept:*/*",
-            "Accept-Language:en-US,en;q=0.9",
-            "Connection:keep-alive",
-            "Content-Length:61",
-            "Content-Type:application/x-www-form-urlencoded; charset=UTF-8",
-            "Cookie:__cfduid=d70de1f6eabeb188f99455a2b86f795301511086344; language=en; PHPSESSID=90a6e4178d3be74bb4fcc8d8778e0c9d; _ga=GA1.2.1372332778.1511086346; _gid=GA1.2.1835651314.1511086346; _gat=1",
-            "Host:csgosell.com",
-            "Origin:https://csgosell.com",
-            "Referer:https://csgosell.com/ru",
-            "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-            "X-Requested-With:XMLHttpRequest"
+            "Host: www.thecsgobot.com",
+            "Connection: keep-alive",
+            "Content-Length: 7",
+            "Accept: */*",
+            "Origin: https://www.thecsgobot.com",
+            "X-Requested-With: XMLHttpRequest",
+            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+            "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer: https://www.thecsgobot.com/trading/",
+//            "Accept-Encoding: gzip, deflate, br",
+            "Accept-Language: en-US,en;q=0.9",
+            "Cookie: __cfduid=d55eded162019ba018906e0d894d5ea701511096482;
+            JSESSIONID=6C5A1642AAF4F09151B09838EDBBF944;
+            _ga=GA1.2.1308276030.1511096483;
+            _gid=GA1.2.437421720.1511096483"
         ));
         $post_data = array (
-            "stage" => "botAll",
-            "steamId" => "76561198364873979",
-            "hasBonus" => "false",
-            "coins" => 0
+            "who" => "bot"
         );
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+//        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, "who=bot");
         $curl_response = curl_exec($curl);
         curl_close($curl);
-        dd(json_decode($curl_response));
+        dd(collect(json_decode($curl_response)->items)->where('descriptions.0.aFloat', '<=', '0.02'));
+//        dd($curl_response);
 
     }
 
