b27b1c1a42d541efb96f8cfca369fedc49270f0a
devpopovegor <popovegordev@mail.ru>
2017-11-24 19:55:54 +0300

    123

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,11 +8,10 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function addItem(Request $request)
+    public function addItem()
     {
-        $name = $request->item_name;
-        $float = $request->item_float;
-        return json_encode($_GET);
+        $name = $_GET['name'];
+        $float = $_GET['float'];
         if ($item = Item::where('name', '=', $name)->first()){
             if (Task::where('item_id', '=', $item->id)->where('client','=','ska4an')
                 ->where('site_id', '=', 7)->where('float', '=', $float)->first()){
