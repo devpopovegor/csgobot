27a1dda16784c1e3a7875d5d1e462c12a5cb780b
devpopovegor <popovegordev@mail.ru>
2017-12-06 20:34:51 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -29,10 +29,10 @@ class TelegramController extends Controller
 
         $paintseeds = [];
         foreach ($tasks as $task){
-            $paintseeds[] = $task->item->patterns->where('name', '=', $task->pattern)->toArray();
-//            foreach ($paterns as $patern){
-//                $paintseeds[] = $patern->value;
-//            }
+            $paterns = $task->item->patterns->where('name', '=', $task->pattern)->toArray();
+            foreach ($paterns as $patern){
+                $paintseeds[] = $patern['value'];
+            }
         }
 
         dd($paintseeds);
