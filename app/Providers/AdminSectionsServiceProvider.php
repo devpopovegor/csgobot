94d6396a1f1522e3208833bc140f6907e4651d41
Егор <egor.popov@roketstation.ru>
2017-12-08 11:23:38 +0300

    add paintseed to admin panel

--- app/Providers/AdminSectionsServiceProvider.php
+++ app/Providers/AdminSectionsServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Dealer;
 //use App\Http\Admin\Sections\Reports;
 use App\Item;
+use App\Paintseed;
 use App\Pattern;
 use App\Report;
 use App\Site;
@@ -26,6 +27,7 @@ class AdminSectionsServiceProvider extends ServiceProvider
         Task::class => 'App\Http\Admin\Sections\Tasks',
         Pattern::class => 'App\Http\Admin\Sections\Patterns',
         Report::class => 'App\Http\Admin\Sections\Reports',
+        Paintseed::class => 'App\Http\Admin\Sections\Paintseeds',
     ];
 
     /**
