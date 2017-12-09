63011a78a62ea1c18683aea6cbd91949131f2335
devpopovegor <popovegordev@mail.ru>
2017-12-09 18:05:20 +0300

    parsing karambit marble

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -23,7 +23,7 @@ class TelegramController extends Controller
 
     public function test(){
         $tasks = Task::with('item')->where('client', '=', 'ska4an')
-            ->where('site_id', '=', '7')->where('pattern', '!=', null)->get();
+            ->where('site_id', '=', '7')->where('pattern', '!=', '')->get();
         echo count($tasks) . "\r\n";
         $paintseeds = [];
         foreach ($tasks as $task){
