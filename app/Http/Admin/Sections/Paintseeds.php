d89b4266f022773c235c4fc1b897c9e5599892f3
devpopovegor <popovegordev@mail.ru>
2017-12-09 20:12:57 +0300

    123

--- app/Http/Admin/Sections/Paintseeds.php
+++ app/Http/Admin/Sections/Paintseeds.php
@@ -56,7 +56,8 @@ class Paintseeds extends Section implements Initializable
 		                   ->setColumns(
 			                   AdminColumn::text('id', '#')->setWidth('30px'),
 			                   AdminColumn::text('item_id', 'steam id'),
-			                   AdminColumn::text('value', 'Паттерн')
+			                   AdminColumn::text('value', 'Паттерн'),
+			                   AdminColumn::text('name', 'Название')
 		                   )->setDisplaySearch(true)->paginate(100);
 	}
 
