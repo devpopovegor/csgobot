63c61716a817b5d79a9f0af2759745a12daf1065
devpopovegor <popovegordev@mail.ru>
2017-12-10 13:09:10 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -50,6 +50,7 @@ class TelegramController extends Controller
         $paindseeds = array_unique($paindseeds);
         $steams = Paintseed::whereIn('value', $paindseeds)->get();
         $steams = $steams->whereIn('name', $names)->toArray();
+        dd(count($steams));
         dd(compact(count($steams),$steams));
 
 
