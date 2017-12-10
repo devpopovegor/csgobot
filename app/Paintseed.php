1f1fda7d5301b5ddcb4a64f9705c621afa046e47
devpopovegor <popovegordev@mail.ru>
2017-12-10 18:09:08 +0300

    123

--- app/Paintseed.php
+++ app/Paintseed.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paintseed extends Model
 {
-    protected $fillable = ['item_id', 'value', 'name'];
+    protected $fillable = ['item_id', 'value', 'name', 'steam_id'];
 
     public function item(){
         return $this->belongsTo(Item::class);
