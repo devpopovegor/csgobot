c51622bf5983497e4e6bd4df244efc86d770a2e1
devpopovegor <popovegordev@mail.ru>
2017-12-10 20:25:02 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -35,8 +35,8 @@ class TelegramController extends Controller
             $arr = [];
             $arr['task'] = $task;
             $arr['patterns'] = array_unique($task->item->patterns->where('name', '=', $task->pattern)->pluck('value')->toArray());
-            $arr['paintseeds'] = $task->item->paintseeds->pluck('value', 'item_id')->toArray();
-            $arr['match'] = array_intersect($arr['paintseeds'], $arr['patterns']);
+            $arr['paintseeds'] = array_unique($task->item->paintseeds->pluck('value')->toArray());
+//            $arr['match'] = array_intersect($arr['paintseeds'], $arr['patterns']);
             $result[] = $arr;
         }
         dd($result[10]);
