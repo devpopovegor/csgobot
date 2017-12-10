a6f7f27050fefaa1510c87d890d0244ea15a331b
devpopovegor <popovegordev@mail.ru>
2017-12-10 18:18:47 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -35,7 +35,7 @@ class TelegramController extends Controller
             $arr = [];
             $arr['task'] = $task;
             $arr['patterns'] = $task->item->patterns->pluck('value')->toArray();
-            $arr['paintseeds'] = $task->item->paintseeds->pluck('item_id')->toArray();
+            $arr['paintseeds'] = $task->item->paintseeds->pluck('item_id', 'value')->toArray();
             $result[] = $arr;
         }
         dd($result[10]);
