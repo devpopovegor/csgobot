cde5c7456e1cbb6aa8b44a1dd19767438ab90b74
devpopovegor <popovegordev@mail.ru>
2017-12-10 13:36:50 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -30,6 +30,9 @@ class TelegramController extends Controller
 //
 //        dd('ok');
 
+        $tasks = json_encode(Task::with('item')->with('patterns')->where('site_id', '=', '7')
+            ->where('client','=', 'ska4an')->get());
+        dd($tasks);
         set_time_limit(0);
 
         $tasks = Task::with('item')->where('client', '=', 'ska4an')
