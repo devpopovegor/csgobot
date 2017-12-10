5915895bfa561625c78bada8c4f9c5934b8b2646
devpopovegor <popovegordev@mail.ru>
2017-12-10 10:16:37 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -43,7 +43,7 @@ class TelegramController extends Controller
 //            dd($paintseeds);
 //        }
         $paintseeds = array_unique($paintseeds);
-        $steam_ids = DB::table('paintseeds')->where('name','=',$tasks[0]->item->name)->pluck('item_id');
+        $steam_ids = DB::table('paintseeds')->where('name','=',$tasks[0]->item->name)->get();
         $steam_ids = $steam_ids->whereIn('value',$paintseeds)->toArray();
         dd(array_unique($paintseeds), $steam_ids, $tasks[0]->item->name);
     }
