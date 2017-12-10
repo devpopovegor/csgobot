ad0a2e00b7b00ec586a2c3de55ca9082a44648d8
devpopovegor <popovegordev@mail.ru>
2017-12-10 10:57:46 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -25,8 +25,8 @@ class TelegramController extends Controller
 
 //        dd("SUCK MY DICK\r\nLICK MY ASS");
 
-        $ps = Paintseed::where('name', '=', '★ StatTrak™ Flip Knife | Marble Fade(Minimal Wear)')
-        ->update(['name' => '★ StatTrak™ Flip Knife | Marble Fade (Minimal Wear)']);
+        $ps = Paintseed::where('name', '=', '★ StatTrak™ Flip Knife | Marble Fade(Factory New)')
+        ->update(['name' => '★ StatTrak™ Flip Knife | Marble Fade (Factory New)']);
 
         dd('ok');
 
