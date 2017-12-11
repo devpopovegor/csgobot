192cc4746d2fbd6ae7de23b0f40b42829ee8f781
Егор <egor.popov@roketstation.ru>
2017-12-11 11:32:05 +0300

    123

--- app/Http/Admin/Sections/Paintseeds.php
+++ app/Http/Admin/Sections/Paintseeds.php
@@ -75,7 +75,9 @@ class Paintseeds extends Section implements Initializable
 	{
 		return AdminForm::panel()->addBody([
 			AdminFormElement::text('item_id', 'steam id'),
-			AdminFormElement::text('value', 'Паттерн')
+			AdminFormElement::text('value', 'Паттерн'),
+			AdminColumn::text('name', 'Название'),
+			AdminColumn::text('steam_id', 'Предмет')
 		]);
 	}
 
