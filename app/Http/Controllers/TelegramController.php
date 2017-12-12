6ebec09484f3a93dda56819c2ab88ccdbb74f4db
devpopovegor <popovegordev@mail.ru>
2017-12-12 20:08:10 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -23,43 +23,24 @@ class TelegramController extends Controller
 
     public function test(){
 
-        dd("SUCK MY DICK\r\nLICK MY ASS");
+//        dd("SUCK MY DICK\r\nLICK MY ASS");
 
         set_time_limit(0);
 
+
         $tasks = Task::with('item')->where('site_id', '=', '7')
-            ->where('client','=', 'ska4an')->get();
+            ->where('pattern','!=', '')->get();
 
         $result = [];
         foreach ($tasks as $task){
             $arr = [];
             $arr['task'] = $task;
             $arr['patterns'] = array_unique($task->item->patterns->where('name', '=', $task->pattern)->pluck('value')->toArray());
-            $arr['paintseeds'] = $task->item->paintseeds->whereIn('value', $arr['patterns'])->pluck('value','item_id')->toArray();
-//            $arr['match'] = array_intersect($arr['paintseeds'], $arr['patterns']);
+            $arr['patterns'] = $task->item->paintseeds->whereIn('value', $arr['patterns'])->pluck('item_id')->toArray();
             $result[] = $arr;
         }
-        dd($result[9]);
 
-        $tasks = Task::with('item')->where('client', '=', 'ska4an')
-            ->where('site_id', '=', '7')->where('pattern', '!=', '')->get();
-        echo count($tasks) . "\r\n";
-
-        $paindseeds = [];
-        $names = [];
-
-        foreach ($tasks as $task){
-            $paterns = $task->item->patterns->where('name', '=', $task->pattern);
-            $names[] = $task->item->name;
-            foreach ($paterns as $patern){
-                $paindseeds[] = $patern->value;
-            }
-        }
-
-        $paindseeds = array_unique($paindseeds);
-        $steams = Paintseed::whereIn('value', $paindseeds)->get();
-        $steams = $steams->whereIn('name', $names)->toArray();
-        dd(json_encode($steams));
+        return dd($result);
 
     }
 
