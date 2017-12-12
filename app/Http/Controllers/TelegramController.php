1f7d36593b77296204f658c466c3a92c8b249ef3
devpopovegor <popovegordev@mail.ru>
2017-12-12 21:23:35 +0300

    123

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -24,7 +24,7 @@ class TelegramController extends Controller
 
     public function test(){
 
-//        dd("SUCK MY DICK\r\nLICK MY ASS");
+        dd("SUCK MY DICK\r\nLICK MY ASS");
 
         set_time_limit(0);
 
