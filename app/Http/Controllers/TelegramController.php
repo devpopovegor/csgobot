6aa94d105333d972cafe2f3d54769e174d948b08
devpopovegor <popovegordev@mail.ru>
2017-12-17 14:04:46 +0300

    paintseeds deleted

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -25,41 +25,15 @@ class TelegramController extends Controller
     public function test(){
 
 
-//        $curl = curl_init();
-//        curl_setopt($curl, CURLOPT_URL, "https://www.thecsgobot.com/api/service.inventory.json");
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($curl, CURLOPT_POST, true);
-//        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-//            "Host: www.thecsgobot.com",
-//            "Connection: keep-alive",
-//            "Content-Length: 7",
-//            "Accept: */*",
-//            "Origin: https://www.thecsgobot.com",
-//            "X-Requested-With: XMLHttpRequest",
-//            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-//            "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
-//            "Referer: https://www.thecsgobot.com/trading/",
-//            "Accept-Language: en-US,en;q=0.9",
-//            "Cookie: __cfduid=d55eded162019ba018906e0d894d5ea701511096482;
-//            JSESSIONID=6C5A1642AAF4F09151B09838EDBBF944;
-//            _ga=GA1.2.1308276030.1511096483;
-//            _gid=GA1.2.437421720.1511096483"
-//        ));
-//        curl_setopt($curl, CURLOPT_POSTFIELDS, "who=bot");
-//        $curl_response = collect(json_decode(curl_exec($curl))->items);
-//        $curl_response = $curl_response->where('descriptions.0.aFloat', '<=', 10)->first();
-//        dd($curl_response);
 
-        dd("SUCK MY DICK\r\nLICK MY ASS");
+//        dd("SUCK MY DICK\r\nLICK MY ASS");
 
         set_time_limit(0);
 
-        $this->set_Steams_task(4);
+//        $this->set_Steams_task(4);
 
-        $tasks = Task::with('item')->with('steams')->where('pattern', '!=', null)->get();
-
-        dd($tasks[10]->steams->pluck('steam_id')->toArray());
-
+        Paintseed::all()->delete();
+        dd('deleted');
     }
 
     private function set_Steams_task($id)
