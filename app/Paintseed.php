1ca518a0b9ac8fbb242de00529a253a03c3617ba
devpopovegor <popovegordev@mail.ru>
2017-12-17 14:46:35 +0300

    paintseeds deleted

--- app/Paintseed.php
+++ app/Paintseed.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paintseed extends Model
 {
-    protected $fillable = ['item_id', 'value', 'name', 'steam_id'];
+    protected $fillable = ['item_id', 'value', 'name', 'steam_id', 'float'];
 
     public function item(){
         return $this->belongsTo(Item::class);
