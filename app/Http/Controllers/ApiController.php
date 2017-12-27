40af91685a46fe0b4197004c2af84ce8c4835d1a
devpopovegor <popovegordev@mail.ru>
2017-12-27 21:39:09 +0300

    123

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -38,7 +38,7 @@ class ApiController extends Controller
     {
         set_time_limit(0);
 
-        $tasks = Task::with(['paintseeds:float', 'item:full_name'])
+        $tasks = Task::with(['paintseeds:float', 'item:name'])
             ->where('site_id','=','7')
             ->where('client','=','ska4an')
             ->get()
